052ac13356a7e197c4ed68e77f56392e0617323d
Abhishek Satpute <abhisheksatpute99@gmail.com>
2022-08-23 16:35:09 +0530

    react app initiate

--- server/src/controller/photoupload.controller.js
+++ server/src/controller/photoupload.controller.js
@@ -10,12 +10,23 @@ router.post("/addphotos",upload.single('p_image'),(req, res)=>{
         p_name: req.body.p_name,
         p_image:req.file.path
     }
-    console.log("data ====" + req.file.path)
     Photos.create(data).then(photos => {
         res.send(photos);
     }).catch(err => {
         res.send(err);
     })
+})
+
+router.get("/photos", (req, res) =>{
+    Photos.findAll({
+
+    }).then(Photos => {
+        res.send(Photos);
+    }
+    ).catch(err => {
+        res.send(err);
+    }
+    );
 })
 
 module.exports = router
